eee8590afab888f24a3b9016ae19668742c398dd
rodrigos21 <rodrigosanchis1@gmail.com>
2022-01-03 16:33:10 -0300

    update

--- app.py
+++ app.py
@@ -14,15 +14,12 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')#'postgresql://username:password@hostname.com')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('postgresql://username:password@hostname.com')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "rodri"
 api = Api(app)
 
-uri = os.getenv("DATABASE_URL")  # or other relevant config var
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-# rest of connection code using the connection string `uri`
+
 
 jwt= JWT(app, authenticate, identity)   #/auth
 
